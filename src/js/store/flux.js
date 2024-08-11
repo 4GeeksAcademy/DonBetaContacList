920ab1470926f85e44ffc0cad3fffd77bafb67ad
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-08-11 11:50:24 +0000

    terminado hasta la tarjeta de contacto

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,32 @@
+const initialState = {
+	contacts: [	]   // estado inicial, que sera con una lista vacia 
+};
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+		store: initialState,
+		action: {
+			// para crear un nuevo contacto
+			addContact: (contact) => {
 				const store = getStore();
+				const newContact = { ...contact, id: store.contacts.length + 1 };
+				setStore({ contacts: [ ...contact, newContact] });
+			},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			// para actualizar un contacto ya creado
+			updateContact: (id, updatedContact) => {
+				const store = getStore();
+				const updatedContacts = store.contacts.map( contact =>
+					contact.id === id ? { ...contact, ...updatedContact } : contact
+				);
+				setStore({ contacts: updatedContacts });
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			//para eliminar un contacto 
+			deleteContact: (id) => {
+				const store = getStore();
+				const updatedContacts = store.contacts.filter(contact => contact.id !== id);
+				setStore({ contacts: updatedContacts });
 			}
 		}
 	};
---
+++ src/js/views/contact.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import ContactCard from "../components/ContactCard";
+
+const Contact = () => {
+    const { store } = useContext(Context);
+
+    return (
+        <div className="container">
+            <h1 className="my-4">Contacts</h1>
+            <div className="row">
+                {store.contacts.length > 0 ?
+                    (store.contacts.map(contact => (
+                        <ContactCard key={contact.id} contact={contact} />
+                    ))
+                    ) : (<p>Sin Contactos en tu lista, Puedes agregar contactos </p>
+
+                    )}
+            </div>
+        </div>
+    );
+};
+export default Contact;
